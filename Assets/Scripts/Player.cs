abda5c3e11db30d986a1364c43aa78acf339851f
DeimisM <deimis.mardosas@outlook.com>
2023-10-01 17:50:17 +0300

    Mechanics
    Game mechanics and visuals

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private void Update()
+    {
+        var mousePos = Camera.main.ScreenToWorldPoint( Input.mousePosition );
+        mousePos.z = 0;
+
+        //transform.position = mousePos;
+        GetComponent<Rigidbody2D>().MovePosition( mousePos );
+    }
+}
