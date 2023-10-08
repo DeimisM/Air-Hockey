cd36d1fdd47f4a6a66fcddb89ae48098bd8f66e4
DeimisM <deimis.mardosas@outlook.com>
2023-10-08 18:38:27 +0300

    Added a menu scene with text and cooldowns

--- Assets/Scripts/Hockey.cs
+++ Assets/Scripts/Hockey.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using System;
+using UnityEngine.SceneManagement;
+using UnityEditor.Search;
+using JetBrains.Annotations;
 
 public class Hockey : MonoBehaviour
 {
@@ -12,10 +15,36 @@ public class Hockey : MonoBehaviour
     public TMP_Text playerScoreText;
     public TMP_Text enemyScoreText;
 
+    public TMP_Text displayText;
+
     //public AudioSource source;
     public AudioClip wallHit;
     public AudioClip hit;
     public AudioClip goal;
+
+    public float respawnCooldown;
+    float respawnTimer;
+
+    public Transform deathPoint;
+    public bool shouldRepspawn;
+
+    Vector3 respawnPosition;
+
+    private void Update()
+    {
+         if (shouldRepspawn)
+         {
+            respawnTimer -= Time.deltaTime;
+            if (respawnTimer <= 0)
+            {
+                transform.position = respawnPosition;
+                respawnTimer = respawnCooldown;
+                shouldRepspawn = false;
+
+                displayText.text = "";
+            }
+         }
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         //GetComponent<AudioSource>().Play();
@@ -25,10 +54,9 @@ public class Hockey : MonoBehaviour
             playerScore++;
             playerScoreText.text = playerScore.ToString();
 
-            transform.position = Vector3.right;
-            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            respawnPosition = Vector3.right;
 
-            GetComponent<AudioSource>().PlayOneShot(goal);
+            displayText.text = "Player Goal!";
         }
 
         if (collision.gameObject.name.Contains("Player Goal"))
@@ -36,10 +64,23 @@ public class Hockey : MonoBehaviour
             enemyScore++;
             enemyScoreText.text = enemyScore.ToString();
 
-            transform.position = Vector3.left;
+            respawnPosition = Vector3.left;
+
+            displayText.text = "Enemy Goal!";
+        }
+
+        if (collision.gameObject.name.Contains("Goal"))
+        {
             GetComponent<Rigidbody2D>().velocity = Vector2.zero;
 
             GetComponent<AudioSource>().PlayOneShot(goal);
+            transform.position = deathPoint.position;
+            shouldRepspawn = true;
+
+            if(playerScore >= 7 || enemyScore >= 7)
+            {
+                SceneManager.LoadScene("Menu");
+            }
         }
 
         if (collision.gameObject.name.Contains("Wall"))
